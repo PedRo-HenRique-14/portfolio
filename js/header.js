2884594d6903f44edbc335e7a2775d70037cb2a1
Dodas_ <ph005009004@gmail.com>
2024-06-25 00:13:38 -0300

    fix: Ajusta responsividade para desktop
    Ajusta responsividade para desktop HD e FullHD

--- js/header.js
+++ js/header.js
@@ -1,18 +1,17 @@
 var headerContent = `
-<header>
-    <div class="logo">
-        <img src="https://avatars.githubusercontent.com/u/53160578?v=4" alt="logo">
-    </div>
-    <nav class="header-nav">
-        <ul class="menu-list">
-            <li class="menu-item"><a href="./index.html">Home</a></li>
-            <li class="menu-item"><a href="./about.html">Sobre</a></li>
-            <li class="menu-item"><a href="">Habilidades</a></li>
-            <li class="menu-item"><a href="">Projetos</a></li>
-            <li class="menu-item"><a href="">Contatos</a></li>
-        </ul>
-    </nav>
-</header>
+<div class="logo">
+    <img src="https://avatars.githubusercontent.com/u/53160578?v=4" alt="logo">
+</div>
+<nav class="header-nav">
+    <ul class="menu-list">
+        <li class="menu-item"><a href="./index.html">Home</a></li>
+        <li class="menu-item"><a href="./about.html">Sobre</a></li>
+        <li class="menu-item"><a href="">Habilidades</a></li>
+        <li class="menu-item"><a href="">Projetos</a></li>
+        <li class="menu-item"><a href="">Contatos</a></li>
+    </ul>
+</nav>
+
 `;
 const header = document.querySelector(".header");
 header.innerHTML = headerContent;
