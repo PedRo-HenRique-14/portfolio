e0fce9f600b401f107f3adbc96a21c057be4e507
Dodas_ <ph005009004@gmail.com>
2024-07-01 14:40:28 -0300

    content: adiciona hobbies e gostoso ao sobre mim

--- js/header/header.js
+++ js/header/header.js
@@ -1,6 +1,6 @@
 var headerContent = `
 <div class="logo">
-    <img src="https://media.licdn.com/dms/image/D4D03AQEl78DG7TQjKg/profile-displayphoto-shrink_800_800/0/1719276281021?e=1724889600&v=beta&t=6IkU5QmHIe-OZ62qixhzU3I2_lyM0UgZ14XLTcRQSJk" alt="logo">
+    <a href='./about.html'><img src="https://media.licdn.com/dms/image/D4D03AQEl78DG7TQjKg/profile-displayphoto-shrink_800_800/0/1719276281021?e=1724889600&v=beta&t=6IkU5QmHIe-OZ62qixhzU3I2_lyM0UgZ14XLTcRQSJk" alt="logo"></a>
 </div>
 <button type="button" class="button-menu"><span>Menu</span><i class="fa-solid fa-chevron-left arrow"></i></button>
 <nav class="header-nav">
